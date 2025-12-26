a4cae6c303583759592ad28f912d8a0937fe0361
Gunj0 <gunjo2012@gmail.com>
2025-12-26 21:30:51 +0900

    サイト情報修正

--- src/const/Site.ts
+++ src/const/Site.ts
@@ -2,5 +2,7 @@ export const SITE = {
   TITLE: "オレの比較メモ",
   DESCRIPTION: "自分だけのガジェットや家電などの比較過程を記録しよう",
   AUTHOR: "Gunj0",
+  URL: "https://my-hikaku-memo.com",
+  OGP: "https://my-hikaku-memo.com/ogp.png",
   YEAR: "2026",
 };
